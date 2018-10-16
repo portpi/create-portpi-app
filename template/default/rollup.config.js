6efc8a06f4b045c8f9bc21d2ec33b28fd08fa8fe
Vivo Xu <vivo@fancyber.com>
2018-10-15 23:31:20 -0700

    fix process.env undefined issue

--- template/default/rollup.config.js
+++ template/default/rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import resolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
 import url from 'rollup-plugin-url';
 import svgr from '@svgr/rollup';
 
@@ -34,6 +35,9 @@ export default {
       plugins: [ 'external-helpers' ]
     }),
     resolve(),
-    commonjs()
+    commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
   ]
 }
